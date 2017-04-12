5f6af5fc3a683112348733946984611e0f959ef1
semistone222@gmail.com <semistone222@gmail.com>
2017-04-12 15:20:40 +0900

    simple move and camera
    간단한 움직임, 점프, 카메라

---
+++ Assets/02.Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Transform tr;
+	private Rigidbody rb;
+
+	void Start () {
+		tr = GetComponent<Transform> ();
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		Move ();
+
+		if (Input.GetKeyDown ("space")) {
+			Jump ();
+		}
+	}
+
+	void Move() {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Debug.Log ("H = " + h.ToString ());
+		Debug.Log ("V = " + v.ToString ());
+
+		Vector3 moveDir = new Vector3(h, 0, v);
+		moveDir *= moveSpeed;
+
+		tr.Translate(moveDir * Time.deltaTime, Space.Self);
+	}
+
+	void Jump() {
+		rb.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
+	}
+}
